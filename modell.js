d17523cf2ff2ced160b2d5eb268de82f3638f2d7
Emmam02 <135839715+Emmam02@users.noreply.github.com>
2023-10-06 14:41:33 +0200

    dddd
    dddd

--- modell.js
+++ modell.js
@@ -3,12 +3,14 @@
 
 //Model
 const defaultView = "mainPage";
+let currentView = model.app.currentView;
 
 const model = {
     app: {
         currentView: defaultView,
         language: "no",
         isDarkMode: false,
+        modeID:0,
     },
     input: {
         searchBar: "",
@@ -16,21 +18,18 @@ const model = {
             username: "",
             password: "",
         },
-
-        createAccount: { //registerView?
+        createAccount: { //registerView
             username: "",
             password: "",
         },
-
-        profileView: {
+        profileView: { //profileView
             bilde: "", //URL Lenke i string (eller eksisterende importert bilde til nettsiden)
             navn: "",
             land: "",
             changePassword: "",
         },
-
-        quizView: {
-            id: "", //<--- Generer en ny id for quizen
+        quizView: { //quizView
+            id: null, //<--- Generer en ny id for quizen
             creator: "", //getUsername();
             title: "", // quiz navn
             bilde: "",  // main quizbilde
@@ -38,12 +37,12 @@ const model = {
             rating: 0, //Hvor høyt den har blitt rangert
             popularitet: 0, // Hvor mange som har tatt quizen
             date: "", //getCurrentDate();
-            questions: [
+            questions: [//Liste med spørsmål
                 {
                     questionID: 0, //id for hver spørsmål
                     theQuestion: "", //Teksten / hva spørsmålet er
                     bilde: "",
-                    answers: [//Maks 4 svar
+                    answers: [//Maks 4 svar <-- Liste med svar innenfor spørsmålet.
                         {
                             answerID: 0,
                             answerText: "",
@@ -56,6 +55,11 @@ const model = {
                         },
                         {
                             answerID: 2,
+                            answerText: "",
+                            color: "",
+                        },
+                        {
+                            answerID: 3,
                             answerText: "",
                             color: "",
                         },
@@ -77,7 +81,6 @@ const model = {
             isPublic: false,
         },
     },
-
     data: {
         newestQuiz: [ //getQuizByDate();
 
@@ -97,7 +100,7 @@ const model = {
         //Lage en eksempelquiz i allQuizes
         allQuizes: [
             {
-                id: "0",
+                id: 0,
                 title: "Hvilken slange er du?",
                 creator: 'Team1C',
                 bilde: "https://i.redd.it/arabian-sand-boa-v0-94vq475nbhj91.jpg?width=425&format=pjpg&auto=webp&s=7b2a3834082baa57d6d10af92315ac45836ed5cd", //quiz hoved bilde
@@ -112,47 +115,47 @@ const model = {
                             {
                                 answerID: 0,
                                 answerText: "Frekk",
-                                color: "1",
+                                color: "0",
                             },
                             {
                                 answerID: 1,
                                 answerText: "Hyggelig",
-                                color: "",
+                                color: "3",
                             },
                             {
                                 answerID: 2,
-                                answerText: "",
-                                color: "",
+                                answerText: "whateverlissom",
+                                color: "1",
                             },
                             {
                                 answerID: 3,
-                                answerText: "",
-                                color: "",
+                                answerText: "Gledesspreder",
+                                color: "2",
                             },
                         ],
                         questionID: 1,
-                        theQuestion: "Hvilken smak liker du best?",
+                        theQuestion: "",
                         bilde: "",
                         answers: [
                             {
                                 answerID: 0,
-                                answerText: "Krokan",
-                                color: "",
+                                answerText: "",
+                                color: "0",
                             },
                             {
                                 answerID: 1,
-                                answerText: "Sjokolade",
+                                answerText: "",
                                 color: "1",
                             },
                             {
                                 answerID: 2,
-                                answerText: "Pecan",
-                                color: "",
+                                answerText: "",
+                                color: "2",
                             },
                             {
                                 answerID: 3,
-                                answerText: "Bringebær",
-                                color: "",
+                                answerText: "",
+                                color: "3",
                             },
                         ],
                     },
@@ -184,7 +187,7 @@ const model = {
             },
             //Ny Quiz starter her
             {
-                id: "1",
+                id: 1,
                 title: "Hvilken wienerbakst er du?",
                 creator: 'Team1C',
                 bilde: "https://www.bakehuset.no/globalassets/bilder/sortiment/smabakst/wienerbakst/10654_wienerpecan.png?width=263&height=190&quality=70", //quiz hoved bilde
@@ -266,15 +269,13 @@ const model = {
                         resultbilde: "https://bilder.ngdata.no/2685/meny/large.jpg", //URL Lenke i string (eller eksisterende importert bilde til nettsiden)
                     },
                 ],
-                isPublic: true,
+                isPublic: true, // er delt
             },
             //Ny quiz starter her
             {
                 
             }
-
         ],
-
         allCategories: [
             "Mat",
             "Dyr",
@@ -283,26 +284,149 @@ const model = {
             "Spill",
             "Annet"
         ],
-
-
+        colors:[
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ],
+        themeMode:[
+            {
+                name: "Light",
+                textColor:"#202020",
+                backgroundColor:"#ffffff",
+            },
+            {
+                name: "Dark",
+                textColor:"#ffffff",
+                backgroundColor:"#181818",
+            },
+        ],
         users: [
             {
                 isDarkMode: false, //<-- Deres preferanse, lastes deretter inn i app isDarkMode
                 username: "Per", //Brukernavnet
+                password: "PerPollIsMyName123", //<-- Unhashed 10/10 passord
                 name: "Per Poll", //Deres navn, om ikke vi gjør firstName LastName
+                land:"",
+                bilde:"",
                 //selfMadeQuizes? En array over hva brukeren har lager eller så kan vi hente data ved hjelp av brukernavnet og legge det inn
                 answeredQuizes: [ //Liste over hva slags quizer de har svart på - referanser til quizzer med ID
                     1, 69, 420
                 ],
             },
+            {
+                isDarkMode: false, //<-- Deres preferanse, lastes deretter inn i app isDarkMode
+                username: "Per", //Brukernavnet
+                password: "PerPollIsMyName123", //<-- Unhashed 10/10 passord
+                name: "Per Poll", //Deres navn, om ikke vi gjør firstName LastName
+                land:"",
+                bilde:"",
+                answeredQuizes: [
+                    1, 69, 420
+                ],
+            },
         ],
-
-
     },
 }
 
+//Her har dere hvordan man oppdaterer viewen når den skal endres. 
+//Gi currentView et av navnene som ligger i case for å så oppdatere det med updateView for å hoppe inn i den nye viewen.
+//currentView = ""; <-- Siden denne ikke har noen string i seg for øyeblikket vil den hoppe i default og kjøre mainView(); 
+//Samme skjer hvis noe får feil navn (pass på uppercase og lowercase) ^
+//currentView = "myProfilePage"; <-- Hopper inn i profileView();
+//updateView();
+
 //View
+function updateView(){
+    switch (model.app.currentView) {
+        //Main Page
+        case "mainPage":
+            mainView();
+            break;
+        //Quiz Page kategorier
+        case "matQuizPage":
+            matQuizView();
+            break;
+        case "dyrQuizPage":
+            matQuizView();
+            break;
+        case "sportQuizPage":
+            matQuizView();
+            break;
+        case "bilQuizPage":
+            matQuizView();
+            break;
+        case "spillQuizPage":
+            matQuizView();
+            break;
+        case "annetQuizPage":
+            annetQuizView();
+            break;
+        //Profil
+        case "editProfilePage":
+            editProfileView();
+            break;
+        case "myProfilePage":
+            profileView();
+            break;
+        case "loginPage":
+            loginView();
+            break;
+        case "registerPage":
+            registerView();
+            break;
+        //Quiz
+        case "createQuizPage":
+            createQuizView();
+            break;
+        case "myQuizPage":
+            myQuizView();
+            break;
+        case "quizPage":
+            quizView();
+            break;
+        //Resultat
+        case "myResultsPage":
+            myResultsView();
+            break;
+        //Default hvis ingen blir funnet
+        default:
+            mainView();
+            break; //<-- Lagt til av auto generering av koden. Trengs ikke her
+    }
+}
+
 //Controller
+
+//dd:mm:yyyy <-- dag, måned, år //ss:mm:hh <-- Sekund, minutt, time (klokke)
+function getCurrentDate(){
+
+}
+
+function getQuizByDate(){
+
+}
+
+function getQuizByPopularity(){
+
+}
+
+function getQuizByRating(){
+
+}
+
+function getUsername(){
+
+}
+
+//Folder MainView
+//i folderen: controlleren til viewen og selve viewen
 
 
 //End
