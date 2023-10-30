c1a180eca4467454359f2c28b15cb493b4f967e0
ivarmar <141323294+ivarmar@users.noreply.github.com>
2023-10-30 15:07:03 +0100

    createQuizViewUpdated
    mangler nesten bare CSS nå.

--- JavaScript/View/createQuizView.js
+++ JavaScript/View/createQuizView.js
@@ -4,79 +4,78 @@ let interactionText = "";
 function createQuizView() {
     app.innerHTML = /*HTML*/ `
         <h1 class="gameQuizContainer"> Create Your own Quiz</h1>
-        <input id="quizTitle" type="text" placeholder="Quiz Name" oninput="${(newQuiz.title =
-            this.value)}" required></input>
-        <select id="quizTheme" oninput="${(newQuiz.theme =
-            this.value)}" required>
-            <option value="Mat">Mat</option>
-            <option value="Dyr">Dyr</option>
-            <option value="Sport">Sport</option>
-            <option value="Biler">Biler</option>
-            <option value="Spill">Spill</option>
-            <option value="Annet">Annet</option>
+        <input id="quizTitle" type="text" placeholder="Quiz Name" oninput="(newQuiz.title =
+            this.value)" required></input>
+        <select id="quizTheme" oninput="(newQuiz.theme =
+            this.value)" required>
+            <option value="${model.data.allCategories[0].name}">Mat</option>
+            <option value="${model.data.allCategories[1].name}">Dyr</option>
+            <option value="${model.data.allCategories[2].name}">Sport</option>
+            <option value="${model.data.allCategories[3].name}">Biler</option>
+            <option value="${model.data.allCategories[4].name}">Spill</option>
+            <option value="${model.data.allCategories[5].name}">Annet</option>
         </select>
         <input id="quizImage" type="text" placeholder="Upload Quiz Image"oninput="${(newQuiz.image =
             this.value)}"required></input>
-        <span id="interactionText"></span>
         <button onclick="clearQuestions()">Add question</button>
         
-        <button onclick="submit()" type="submit">Submit</button>
         <label class="switch">
         <input onclick="togglePrivate()" type="checkbox">
         <span class="slider round"></span>
         </label>
         <div id="togglePrivate" class="private"> Private/Public</div>
             
-        <div>
-            <input type="text" placeholder="Question" oninput="${(newQuiz.questions[0].theQuestion =
-                this.value)}" >
-            <input type="text" placeholder="Upload Question Image" oninput="${(newQuiz.questions[0].questionImage =
-                this.value)}"></input>
+        <div id="questions-container">
+        <!-- hsnush -->
+            <input type="text" placeholder="Question" oninput="(newQuiz.questions[0].theQuestion =
+                this.value)" >
+            <input type="text" placeholder="Upload Question Image" oninput="(newQuiz.questions[0].questionImage =
+                this.value)"></input>
             <div class="answer">
-            <input type="text" placeholder="Answer1" oninput="${(newQuiz.questions[0].answers[0].answerText =
-                this.value)}"></input>
-                <select onchange="${(newQuiz.questions[0].answers[0].color =
-                    this.value)}">
-                    <option value="0">Red</option>
-                    <option value="1">Blue</option>
-                    <option value="2">Yellow</option>
-                    <option value="3">Orange</option>
+            <input type="text" placeholder="Answer1" oninput="(newQuiz.questions[0].answers[0].answerText =
+                this.value)"></input>
+                <select onchange="(newQuiz.questions[0].answers[0].color =
+                    this.value)">
+                    <option value="${model.data.colors[0]}">Red</option>
+                    <option value="${model.data.colors[1]}">Blue</option>
+                    <option value="${model.data.colors[2]}">Yellow</option>
+                    <option value="${model.data.colors[3]}">Orange</option>
                 </select>
             </div>
             <div class="answer">
-                <input type="text" placeholder="Answer2"oninput="${(newQuiz.questions[0].answers[1].answerText =
-                    this.value)}"></input>
-                <select onchange="${(newQuiz.questions[0].answers[1].color =
-                    this.value)}">
-                    <option value="0">Red</option>
-                    <option value="1">Blue</option>
-                    <option value="2">Yellow</option>
-                    <option value="3">Orange</option>
+                <input type="text" placeholder="Answer2"oninput="(newQuiz.questions[0].answers[1].answerText =
+                    this.value)"></input>
+                <select onchange="(newQuiz.questions[0].answers[1].color =
+                    this.value)">
+                    <option value="${model.data.colors[0]}">Red</option>
+                    <option value="${model.data.colors[1]}">Blue</option>
+                    <option value="${model.data.colors[2]}">Yellow</option>
+                    <option value="${model.data.colors[3]}">Orange</option>
                 </select>
             </div>
             <div class="answer">
-                <input type="text" placeholder="Answer3"oninput="${(newQuiz.questions[0].answers[2].answerText =
-                    this.value)}"></input>
-                <select onchange="${(newQuiz.questions[0].answers[2].color =
-                    this.value)}">
-                    <option value="0">Red</option>
-                    <option value="1">Blue</option>
-                    <option value="2">Yellow</option>
-                    <option value="3">Orange</option>
+                <input type="text" placeholder="Answer3"oninput="(newQuiz.questions[0].answers[2].answerText =
+                    this.value)"></input>
+                <select onchange="(newQuiz.questions[0].answers[2].color =
+                    this.value)">
+                    <option value="${model.data.colors[0]}">Red</option>
+                    <option value="${model.data.colors[1]}">Blue</option>
+                    <option value="${model.data.colors[2]}">Yellow</option>
+                    <option value="${model.data.colors[3]}">Orange</option>
                 </select>
             </div>
             <div class="answer">
-                <input type="text" placeholder="Answer4" oninput="${(newQuiz.questions[0].answers[3].answerText =
-                    this.value)}"></input>
-                <select onchange="${(newQuiz.questions[0].answers[3].color =
-                    this.value)}">
-                    <option value="0">Red</option>
-                    <option value="1">Blue</option>
-                    <option value="2">Yellow</option>
-                    <option value="3">Orange</option>
+                <input type="text" placeholder="Answer4" oninput="(newQuiz.questions[0].answers[3].answerText =
+                    this.value)"></input>
+                <select onchange="(newQuiz.questions[0].answers[3].color =
+                    this.value)">
+                    <option value="${model.data.colors[0]}">Red</option>
+                    <option value="${model.data.colors[1]}">Blue</option>
+                    <option value="${model.data.colors[2]}">Yellow</option>
+                    <option value="${model.data.colors[3]}">Orange</option>
                 </select>
             </div>
-        
+            <!-- hsnush -->
         </div>
         
         
@@ -85,30 +84,33 @@ function createQuizView() {
 
     <div id="resultsContainer">
         <div id="resultRed">Result Red:
-            <input oninput="${(newQuiz.results[0].result =
-                this.value)}" placeholder="result text"></input>
-            <input oninput="${(newQuiz.results[0].resultimage =
-                this.value)}" placeholder="result image"></input>
+            <input oninput="(newQuiz.results[0].result =
+                this.value)" placeholder="result text"></input>
+            <input oninput="(newQuiz.results[0].resultimage =
+                this.value)" placeholder="result image"></input>
         </div>
         <div id="resultBlue">Result Blue:
-            <input oninput="${(newQuiz.results[1].result =
-                this.value)}" placeholder="result text"></input>
-            <input oninput="${(newQuiz.results[1].resultimage =
-                this.value)}" placeholder="result image"></input>
+            <input oninput="(newQuiz.results[1].result =
+                this.value)" placeholder="result text"></input>
+            <input oninput="(newQuiz.results[1].resultimage =
+                this.value)" placeholder="result image"></input>
         </div>
         <div id="resultYellow">Result Yellow:
-            <input oninput="${(newQuiz.results[2].result =
-                this.value)}" placeholder="result text"></input>
-            <input oninput="${(newQuiz.results[2].resultimage =
-                this.value)}" placeholder="result image"></input>
+            <input oninput="(newQuiz.results[2].result =
+                this.value)" placeholder="result text"></input>
+            <input oninput="(newQuiz.results[2].resultimage =
+                this.value)" placeholder="result image"></input>
         </div>
         <div id="resultOrange">Result Orange:
-            <input oninput="${(newQuiz.results[3].result =
-                this.value)}" placeholder="result text"></input>
-            <input oninput="${(newQuiz.results[3].resultimage =
-                this.value)}" placeholder="result image"></input>
+            <input oninput="(newQuiz.results[3].result =
+                this.value)" placeholder="result text"></input>
+            <input oninput="(newQuiz.results[3].resultimage =
+                this.value)" placeholder="result image"></input>
         </div>
     </div>
+    <span id="interactionText"></span>
+    <button onclick="submit()" type="submit">Submit</button>
+
     `;
 }
 
@@ -123,8 +125,73 @@ function togglePrivate() {
     }
 }
 
+function newQuestions() {
+    const createQuizContainer = document.getElementById("questions-container");
+    createQuizContainer.innerHTML = /*HTML*/ `
+    <input type="text" placeholder="Question" oninput="(newQuiz.questions[0].theQuestion =
+        this.value)" >
+    <input type="text" placeholder="Upload Question Image" oninput="(newQuiz.questions[0].questionImage =
+        this.value)"></input>
+    <div class="answer">
+    <input type="text" placeholder="Answer1" oninput="(newQuiz.questions[0].answers[0].answerText =
+        this.value)"></input>
+        <select onchange="(newQuiz.questions[0].answers[0].color =
+            this.value)">
+            <option value="${model.data.colors[0]}">Red</option>
+            <option value="${model.data.colors[1]}">Blue</option>
+            <option value="${model.data.colors[2]}">Yellow</option>
+            <option value="${model.data.colors[3]}">Orange</option>
+        </select>
+    </div>
+    <div class="answer">
+        <input type="text" placeholder="Answer2"oninput="(newQuiz.questions[0].answers[1].answerText =
+            this.value)"></input>
+        <select onchange="(newQuiz.questions[0].answers[1].color =
+            this.value)">
+            <option value="${model.data.colors[0]}">Red</option>
+            <option value="${model.data.colors[1]}">Blue</option>
+            <option value="${model.data.colors[2]}">Yellow</option>
+            <option value="${model.data.colors[3]}">Orange</option>
+        </select>
+    </div>
+    <div class="answer">
+        <input type="text" placeholder="Answer3"oninput="(newQuiz.questions[0].answers[2].answerText =
+            this.value)"></input>
+        <select onchange="(newQuiz.questions[0].answers[2].color =
+            this.value)">
+            <option value="${model.data.colors[0]}">Red</option>
+            <option value="${model.data.colors[1]}">Blue</option>
+            <option value="${model.data.colors[2]}">Yellow</option>
+            <option value="${model.data.colors[3]}">Orange</option>
+        </select>
+    </div>
+    <div class="answer">
+        <input type="text" placeholder="Answer4" oninput="(newQuiz.questions[0].answers[3].answerText =
+            this.value)"></input>
+        <select onchange="(newQuiz.questions[0].answers[3].color =
+            this.value)">
+            <option value="${model.data.colors[0]}">Red</option>
+            <option value="${model.data.colors[1]}">Blue</option>
+            <option value="${model.data.colors[2]}">Yellow</option>
+            <option value="${model.data.colors[3]}">Orange</option>
+        </select>
+    </div>
+    <!-- hsnush -->
+    `;
+}
+
+function clearThis(target) {
+    document.getElementById(target).innerHTML = "";
+    //target.value = "";
+}
 function clearQuestions() {
     newQuiz.questions.push(newQuiz.questions[0]);
+
+    //10
+    //21
+
+    clearThis("questions-container");
+    newQuestions();
 
     newQuiz.questions[0] = {
         questionID: null,
@@ -153,6 +220,9 @@ function clearQuestions() {
             },
         ],
     };
+
+    console.log("Test: " + newQuiz.theme);
+
     setInteractionText(`Question Added Successfully`);
     setTimeout(() => {
         setInteractionText("");
@@ -172,6 +242,9 @@ function submit() {
     setTimeout(() => {
         setInteractionText("");
     }, 2000);
+    model.app.currentQuizId = newQuiz.id;
+    getQuiz();
+    quizView();
 }
 
 function generateUniqueId() {
@@ -224,7 +297,7 @@ function clearNewQuiz() {
         ],
         isPublic: true,
     };
-    setInteractionText(`Quiz Added Successfully`);
+    setInteractionText(`Question Added Successfully`);
     setTimeout(() => {
         setInteractionText("");
     }, 2000);
