f692bbcc8f2117160696db436d40c2dc27b09672
Emmam02 <135839715+Emmam02@users.noreply.github.com>
2023-10-31 12:22:57 +0100

    colorchange
    ddddd

--- JavaScript/View/createQuizView.js
+++ JavaScript/View/createQuizView.js
@@ -2,12 +2,12 @@ let newQuiz = model.input.createQuizView;
 let interactionText = "";
 
 function createQuizView() {
-    app.innerHTML = /*HTML*/ `
+  app.innerHTML = /*HTML*/ `
         <h1 class="gameQuizContainer"> Create Your own Quiz</h1>
         <input id="quizTitle" type="text" placeholder="Quiz Name" oninput="${(newQuiz.title =
-            this.value)}" required></input>
+          this.value)}" required></input>
         <select id="quizTheme" oninput="${(newQuiz.theme =
-            this.value)}" required>
+          this.value)}" required>
             <option value="Mat">Mat</option>
             <option value="Dyr">Dyr</option>
             <option value="Sport">Sport</option>
@@ -16,7 +16,7 @@ function createQuizView() {
             <option value="Annet">Annet</option>
         </select>
         <input id="quizImage" type="text" placeholder="Upload Quiz Image"oninput="${(newQuiz.image =
-            this.value)}"required></input>
+          this.value)}"required></input>
         <span id="interactionText"></span>
         <button onclick="clearQuestions()">Add question</button>
         
@@ -29,14 +29,14 @@ function createQuizView() {
             
         <div>
             <input type="text" placeholder="Question" oninput="${(newQuiz.questions[0].theQuestion =
-                this.value)}" >
+              this.value)}" >
             <input type="text" placeholder="Upload Question Image" oninput="${(newQuiz.questions[0].questionImage =
-                this.value)}"></input>
+              this.value)}"></input>
             <div class="answer">
             <input type="text" placeholder="Answer1" oninput="${(newQuiz.questions[0].answers[0].answerText =
-                this.value)}"></input>
+              this.value)}"></input>
                 <select onchange="${(newQuiz.questions[0].answers[0].color =
-                    this.value)}">
+                  this.value)}">
                     <option value="0">Red</option>
                     <option value="1">Blue</option>
                     <option value="2">Yellow</option>
@@ -45,9 +45,9 @@ function createQuizView() {
             </div>
             <div class="answer">
                 <input type="text" placeholder="Answer2"oninput="${(newQuiz.questions[0].answers[1].answerText =
-                    this.value)}"></input>
+                  this.value)}"></input>
                 <select onchange="${(newQuiz.questions[0].answers[1].color =
-                    this.value)}">
+                  this.value)}">
                     <option value="0">Red</option>
                     <option value="1">Blue</option>
                     <option value="2">Yellow</option>
@@ -56,9 +56,9 @@ function createQuizView() {
             </div>
             <div class="answer">
                 <input type="text" placeholder="Answer3"oninput="${(newQuiz.questions[0].answers[2].answerText =
-                    this.value)}"></input>
+                  this.value)}"></input>
                 <select onchange="${(newQuiz.questions[0].answers[2].color =
-                    this.value)}">
+                  this.value)}">
                     <option value="0">Red</option>
                     <option value="1">Blue</option>
                     <option value="2">Yellow</option>
@@ -67,9 +67,9 @@ function createQuizView() {
             </div>
             <div class="answer">
                 <input type="text" placeholder="Answer4" oninput="${(newQuiz.questions[0].answers[3].answerText =
-                    this.value)}"></input>
+                  this.value)}"></input>
                 <select onchange="${(newQuiz.questions[0].answers[3].color =
-                    this.value)}">
+                  this.value)}">
                     <option value="0">Red</option>
                     <option value="1">Blue</option>
                     <option value="2">Yellow</option>
@@ -86,146 +86,146 @@ function createQuizView() {
     <div id="resultsContainer">
         <div id="resultRed">Result Red:
             <input oninput="${(newQuiz.results[0].result =
-                this.value)}" placeholder="result text"></input>
+              this.value)}" placeholder="result text"></input>
             <input oninput="${(newQuiz.results[0].resultimage =
-                this.value)}" placeholder="result image"></input>
+              this.value)}" placeholder="result image"></input>
         </div>
         <div id="resultBlue">Result Blue:
             <input oninput="${(newQuiz.results[1].result =
-                this.value)}" placeholder="result text"></input>
+              this.value)}" placeholder="result text"></input>
             <input oninput="${(newQuiz.results[1].resultimage =
-                this.value)}" placeholder="result image"></input>
+              this.value)}" placeholder="result image"></input>
         </div>
         <div id="resultYellow">Result Yellow:
             <input oninput="${(newQuiz.results[2].result =
-                this.value)}" placeholder="result text"></input>
+              this.value)}" placeholder="result text"></input>
             <input oninput="${(newQuiz.results[2].resultimage =
-                this.value)}" placeholder="result image"></input>
+              this.value)}" placeholder="result image"></input>
         </div>
         <div id="resultOrange">Result Orange:
             <input oninput="${(newQuiz.results[3].result =
-                this.value)}" placeholder="result text"></input>
+              this.value)}" placeholder="result text"></input>
             <input oninput="${(newQuiz.results[3].resultimage =
-                this.value)}" placeholder="result image"></input>
+              this.value)}" placeholder="result image"></input>
         </div>
     </div>
     `;
 }
 
 function togglePrivate() {
-    let togglePrivate = document.getElementById("togglePrivate");
-    if (newQuiz.isPublic == false) {
-        newQuiz.isPublic = true;
-        togglePrivate.innerText = `Public`;
-    } else if (newQuiz.isPublic == true) {
-        togglePrivate.innerText = `Private`;
-        newQuiz.isPublic = false;
-    }
+  let togglePrivate = document.getElementById("togglePrivate");
+  if (newQuiz.isPublic == false) {
+    newQuiz.isPublic = true;
+    togglePrivate.innerText = `Public`;
+  } else if (newQuiz.isPublic == true) {
+    togglePrivate.innerText = `Private`;
+    newQuiz.isPublic = false;
+  }
 }
 
 function clearQuestions() {
-    newQuiz.questions.push(newQuiz.questions[0]);
+  newQuiz.questions.push(newQuiz.questions[0]);
 
-    newQuiz.questions[0] = {
+  newQuiz.questions[0] = {
+    questionID: null,
+    theQuestion: "",
+    questionImage: "",
+    answers: [
+      {
+        answerID: null,
+        answerText: " ",
+        color: "",
+      },
+      {
+        answerID: null,
+        answerText: "",
+        color: "",
+      },
+      {
+        answerID: null,
+        answerText: "",
+        color: "",
+      },
+      {
+        answerID: null,
+        answerText: "",
+        color: "",
+      },
+    ],
+  };
+  setInteractionText(`Question Added Successfully`);
+  setTimeout(() => {
+    setInteractionText("");
+  }, 2000);
+}
+
+function setInteractionText(text) {
+  document.getElementById("interactionText").innerText = text;
+}
+
+function submit() {
+  newQuiz.id = generateUniqueId();
+  newQuiz.creator = getUsername();
+  newQuiz.date = getCurrentDate();
+  model.data.allQuizes.push(newQuiz);
+  setInteractionText(`Quiz Added Successfully`);
+  setTimeout(() => {
+    setInteractionText("");
+  }, 2000);
+}
+
+function generateUniqueId() {
+  return Math.floor(Math.random() * Date.now());
+}
+
+function clearNewQuiz() {
+  newQuiz = {
+    id: null,
+    title: "",
+    creator: "",
+    image: "",
+    theme: "",
+    date: "",
+    questions: [
+      {
         questionID: null,
         theQuestion: "",
         questionImage: "",
         answers: [
-            {
-                answerID: null,
-                answerText: " ",
-                color: "",
-            },
-            {
-                answerID: null,
-                answerText: "",
-                color: "",
-            },
-            {
-                answerID: null,
-                answerText: "",
-                color: "",
-            },
-            {
-                answerID: null,
-                answerText: "",
-                color: "",
-            },
+          {
+            answerID: null,
+            answerText: " ",
+            color: "",
+          },
+          {
+            answerID: null,
+            answerText: "",
+            color: "",
+          },
+          {
+            answerID: null,
+            answerText: "",
+            color: "",
+          },
+          {
+            answerID: null,
+            answerText: "",
+            color: "",
+          },
         ],
-    };
-    setInteractionText(`Question Added Successfully`);
-    setTimeout(() => {
-        setInteractionText("");
-    }, 2000);
-}
-
-function setInteractionText(text) {
-    document.getElementById("interactionText").innerText = text;
-}
-
-function submit() {
-    newQuiz.id = generateUniqueId();
-    newQuiz.creator = getUsername();
-    newQuiz.date = getCurrentDate();
-    model.data.allQuizes.push(newQuiz);
-    setInteractionText(`Quiz Added Successfully`);
-    setTimeout(() => {
-        setInteractionText("");
-    }, 2000);
-}
-
-function generateUniqueId() {
-    return Math.floor(Math.random() * Date.now());
-}
-
-function clearNewQuiz() {
-    newQuiz = {
+      },
+    ],
+    results: [
+      {
         id: null,
-        title: "",
-        creator: "",
-        image: "",
-        theme: "",
-        date: "",
-        questions: [
-            {
-                questionID: null,
-                theQuestion: "",
-                questionImage: "",
-                answers: [
-                    {
-                        answerID: null,
-                        answerText: " ",
-                        color: "",
-                    },
-                    {
-                        answerID: null,
-                        answerText: "",
-                        color: "",
-                    },
-                    {
-                        answerID: null,
-                        answerText: "",
-                        color: "",
-                    },
-                    {
-                        answerID: null,
-                        answerText: "",
-                        color: "",
-                    },
-                ],
-            },
-        ],
-        results: [
-            {
-                id: null,
-                result: "",
-                resultimage: "", //URL Lenke i string (eller eksisterende importert bilde til nettsiden)
-            },
-        ],
-        isPublic: true,
-    };
-    setInteractionText(`Quiz Added Successfully`);
-    setTimeout(() => {
-        setInteractionText("");
-    }, 2000);
+        result: "",
+        resultimage: "", //URL Lenke i string (eller eksisterende importert bilde til nettsiden)
+      },
+    ],
+    isPublic: true,
+  };
+  setInteractionText(`Quiz Added Successfully`);
+  setTimeout(() => {
+    setInteractionText("");
+  }, 2000);
 }
