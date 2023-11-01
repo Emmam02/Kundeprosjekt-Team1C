5facdeb57b867c2e27e812ccf26c35fdac0617f7
Eyes of The Serpent <141904493+EyesOfTheSerpent@users.noreply.github.com>
2023-11-01 14:58:44 +0100

    Adjusted the model
    November starter tungt

--- JavaScript/View/QuizFilterView.js
+++ JavaScript/View/QuizFilterView.js
@@ -6,6 +6,17 @@ function quizFilterView(){
     <div id="quizFilterSite">
         <div>Hei, denne siden er tom. :></div>
         <div class="link-view" onclick="changeView('mainView')">Tilbake<div>
+
+        <div id=extra-container>
+            <div id="theQuiz-container">
+                <div id="newest-Quiz"></div>
+                <div id="popular-Quiz"></div>
+            </div>
+            <div id="">
+                <div id=""></div>
+                <div id=""></div>
+            </div>
+        </div>
     </div>
     `;
 }
