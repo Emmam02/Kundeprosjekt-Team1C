3d16bd09c85cbacf01852cfaf209cbd8b34844ce
Eyes of The Serpent <141904493+EyesOfTheSerpent@users.noreply.github.com>
2023-11-01 09:27:44 +0100

    Sanity = -1
    w409y8hwf97uwfeyt768e4t

--- JavaScript/View/MainView.js
+++ JavaScript/View/MainView.js
@@ -6,7 +6,7 @@ function mainView(){
     <div id="mainView">
         <nav id="nav-bar">
             <div id="nav-title-content">
-                <div id="nav-title">Testify - Main Menu</div>
+                <div id="nav-title">Testify - Hovedmeny</div>
             </div>
             <div id="nav-profile-content">
                 <div id="nav-profile-text"></div>
@@ -15,9 +15,10 @@ function mainView(){
         </nav>
 
         <div id="search-bar">
-            <input id="search-bar-text" placeholder="Search..." type="search">
+            <input id="search-bar-text" placeholder="Søk..." type="search">
         </div>
 
+        <span>
         <div id="themeContainer">
             <div class="themeDisplay link-view" onclick="changeView('matQuizView')">${model.data.allCategories[0].name}</div>
             <div class="themeDisplay link-view" onclick="changeView('dyrQuizView')">${model.data.allCategories[1].name}</div>
@@ -27,22 +28,20 @@ function mainView(){
             <div class="themeDisplay link-view" onclick="changeView('annetQuizView')">${model.data.allCategories[5].name}</div>
         </div>
 
-
         <div id="quiz-box-position">
             <div id="quiz-by-date" class=""><div>
             <div id="quiz-by-date" class=""><div>
         </div>
 
         <div id="temp-container">
-            <h1>Site under construction</h1>
-            <p style="color: #ca4848;">Please utilize these links below.</p>
+            <h1>Site er under oppbygging</h1>
+            <p style="color: #ca4848;">Vennligst bruk linkene under.</p>
             <p class="link-view" onclick="changeView('loginView')">LoginView</p>
             <p class="link-view" onclick="changeView('registerView')">RegisterView</p>
             <p class="link-view" onclick="changeView('createQuizView')">CreateQuizView</p>
-            <p class="link-view" onclick="changeView('myProfileView')">profileView</p>
+            <p class="link-view" onclick="changeView('myProfileView')">ProfileView</p>
             <p class="link-view" onclick="changeView('quizView')">QuizView</p>
-            <p class="link-view" onclick="changeView('')">Temp</p>
-            <p class="link-view" onclick="changeView('')">Temp</p>
+            <p class="link-view" onclick="changeView('quizFilterView')">QuizFilterView</p>
         </div>
     </div>
     `;
--- JavaScript/View/QuizFilterView.js
+++ JavaScript/View/QuizFilterView.js
@@ -3,8 +3,9 @@
 
 function quizFilterView(){
     app.innerHTML = /*HTML*/`
-    <div id="ghsue">
-
+    <div id="quizFilterSite">
+        <div>Hei, denne siden er tom. :></div>
+        <div class="link-view" onclick="changeView('mainView')">Tilbake<div>
     </div>
     `;
 }
